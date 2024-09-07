749c7157a1c721e4707343ff5a33e35e6c1193bb
renatrysaev <renat.rysaev@gmail.com>
2024-09-07 05:29:05 +0500

    Enable cors.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,8 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
-  
+  app.enableCors({
+    origin: [
+        'http://localhost:8000',
+        'https://rysaev-diploma.nomorepartiesco.ru'
+    ],
+  });
+
   await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();
